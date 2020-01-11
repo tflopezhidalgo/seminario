aa3231b915cb7b8cf1e2b06efde655895ea31976
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2020-01-11 17:17:51 -0300

    Agrego más clases de dominio

--- previApp/grails-app/domain/previapp/Lugar.groovy
+++ previApp/grails-app/domain/previapp/Lugar.groovy
@@ -2,6 +2,19 @@ package previapp
 
 class Lugar {
 
-    static constraints = {
-    }
+	String nombre
+	String direccion
+	Integer puntuacion
+	/* TODO: Agregar atributo de horario de atención */
+	Integer capacidad_maxima
+
+    static constraints = {}
+
+	Lugar(String nombre, String direccion, Integer puntuacion = 0, Integer capacidad_maxima) {
+		this.nombre = nombre
+		this.direccion = direccion
+		this.puntuacion = puntuacion
+		this.capacidad_maxima = capacidad_maxima
+	}
+
 }
