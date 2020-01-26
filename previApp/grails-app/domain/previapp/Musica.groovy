c630d3aad7aa0b4d286511835062cf448fd858fa
Juan <juanopizzi@gmail.com>
2020-01-26 16:35:48 -0300

    agrego funcionalidad de musica

--- previApp/grails-app/domain/previapp/Musica.groovy
+++ previApp/grails-app/domain/previapp/Musica.groovy
@@ -1,9 +1,25 @@
 package previapp
 
 class Musica {
-
+	List subgeneros
 	String genero_musical
 
     static constraints = {
     }
+
+    Musica(ArrayList subgeneros_musicales, String nombre) {
+		this.subgeneros = new ArrayList<Zona>()
+		subgeneros_musicales.each { subgenero ->
+			this.subgeneros << subgenero
+		}
+		this.genero_musical = nombre
+	}
+
+	def agregarSubgenero(Musica subgenero) {
+		this.subgeneros << subgenero
+	}
+
+	def getSubgeneros() {
+		return this.subgeneros
+	}
 }
