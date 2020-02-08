b212e68dfc0b90abf5e139a5b5168d7ea0be2256
Juan <juanopizzi@gmail.com>
2020-02-08 16:24:11 -0300

    agrego 3 tests unitarios de lugar

--- previApp/src/test/groovy/previapp/LugarSpec.groovy
+++ previApp/src/test/groovy/previapp/LugarSpec.groovy
@@ -5,14 +5,31 @@ import spock.lang.Specification
 
 class LugarSpec extends Specification implements DomainUnitTest<Lugar> {
 
-    def setup() {
+    //def setup() {}
+
+    def cleanup() {}
+
+    void "se crea un lugar para chequear el barrio"() {
+        when "se crea un lugar en Palermo"
+            Lugar lugar = new Lugar('Palermo', 'nvm', 200)
+
+        then "chequeo el lugar"
+            lugar.nombre == "Palermo"
     }
 
-    def cleanup() {
+    void "se crea un lugar para chequear la direccion"() {
+        when "se crea un lugar con direccion en Larralde"
+            Lugar lugar = new Lugar('Palermo', 'Larralde 2500', 200)
+
+        then "chequeo la direccion"
+            lugar.direccion == "Larralde 2500"
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "se crea un lugar con capacidad de 125 personas"() {
+        when "se crea un lugar con dicha capacidad"
+            Lugar lugar = new Lugar('Palermo', 'nvm', 125)
+
+        then "chequeo la capacidad"
+            lugar.capacidadMaxima == 125
     }
 }
