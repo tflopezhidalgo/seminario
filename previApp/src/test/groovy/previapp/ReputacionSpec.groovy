2be0d1cabb034ee526c9732a54907781ea47e9fd
Juan <juanopizzi@gmail.com>
2020-02-09 00:42:09 -0300

    agrego tests de clase Reputacion

--- previApp/src/test/groovy/previapp/ReputacionSpec.groovy
+++ previApp/src/test/groovy/previapp/ReputacionSpec.groovy
@@ -11,8 +11,38 @@ class ReputacionSpec extends Specification implements DomainUnitTest<Reputacion>
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "se crea una Reputacion con estado inicial valido"() {
+        when: "creo una Reputacion"
+        Reputacion reputacion = new Reputacion()
+
+        then: "chequeo que inicialize bien"
+        reputacion.puntos == 0 && reputacion.nombreReputacion == 'Bronce'
+    }
+
+    void "se cargan puntos a una Reputacion"() {
+        when: "creo una Reputacion y le actualizo puntos"
+        Reputacion reputacion = new Reputacion()
+        reputacion.sumarPuntos(30)
+
+        then: "chequeo que el puntaje se cargue bien"
+        reputacion.puntos == 30
+    }
+
+    void "convierto una Reputacion a Plata"() {
+        when: "creo una Reputacion y la subo de nivel"
+        Reputacion reputacion = new Reputacion()
+        reputacion.sumarPuntos(60)
+
+        then: "chequeo que sea de plata"
+        reputacion.nombreReputacion == 'Plata'
+    }
+
+    void "convierto una Reputacion a Oro"() {
+        when: "creo una Reputacion y la subo de nivel 2 veces"
+        Reputacion reputacion = new Reputacion()
+        reputacion.sumarPuntos(160)
+
+        then: "chequeo que sea de oro"
+        reputacion.nombreReputacion == 'Oro'
     }
 }
