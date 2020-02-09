1a594ae12a3c75c68f5fe3ea2d2a9efcce2ccaf2
Juan <juanopizzi@gmail.com>
2020-02-09 12:00:37 -0300

    agrego tests de clase Presupuesto

--- previApp/src/test/groovy/previapp/PresupuestoSpec.groovy
+++ previApp/src/test/groovy/previapp/PresupuestoSpec.groovy
@@ -11,8 +11,29 @@ class PresupuestoSpec extends Specification implements DomainUnitTest<Presupuest
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "Test de constructor de presupuesto"() {
+		when:"Creo un presupuesto"
+		Presupuesto presupuesto = new Presupuesto(15)
+
+        then:"veo que se creo bien"
+        presupuesto.monto_presupuesto == 15
     }
+
+    void "Test de exceder presupuesto"() {
+		when:"Creo un genero presupuesto"
+		Presupuesto presupuesto = new Presupuesto(50)
+
+        then:"veo que me este excediendo del presupuesto"
+        presupuesto.excediPresupuesto(100) == true
+    }
+
+    void "Test de NO exceder presupuesto"() {
+		when:"Creo un genero presupuesto"
+		Presupuesto presupuesto = new Presupuesto(50)
+
+        then:"veo que NO me este excediendo del presupuesto"
+        presupuesto.excediPresupuesto(10) == false
+    }
+
+    //ToDo: testear el Lugar excede presupuesto cuando este implementado
 }
